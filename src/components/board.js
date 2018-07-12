dca562460f0b2f033160e7e932130726c4b01a58
Ben Bridges <ben2d2@gmail.com>
2018-07-12 11:07:54 -0500

    Rename winningSquare prop and css class

--- src/components/board.js
+++ src/components/board.js
@@ -2,27 +2,23 @@ import React from 'react';
 import Square from './square.js';
 
 class Board extends React.Component {
-  renderSquare(i, winner) {
+  renderSquare(i, winningSquare) {
     return <Square
       value={this.props.squares[i]}
       onClick={() => this.props.onClick(i)}
       key={i}
-      winner={winner}
+      winningSquare={winningSquare}
     />;
   }
 
   renderRows() {
-    var rows = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8]
-    ];
+    var rows = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
 
     return rows.map(function(row, index){
       return (<div className="board-row" key={"row-" + index}>{
         row.map(function(i) {
-          var winner = this.props.winningSquares ? this.props.winningSquares.includes(i) : false
-          return this.renderSquare(i, winner);
+          var winningSquare = this.props.winningSquares ? this.props.winningSquares.includes(i) : false
+          return this.renderSquare(i, winningSquare);
         }, this)
       }</div>)
     }, this);
--- src/components/square.js
+++ src/components/square.js
@@ -3,7 +3,10 @@ import React from 'react';
 class Square extends React.Component {
   render() {
     return (
-      <button className={this.props.winner ? 'square winner' : 'square'} onClick={this.props.onClick}>
+      <button
+        className={this.props.winningSquare ? 'square winning-square' : 'square'}
+        onClick={this.props.onClick}
+      >
         {this.props.value}
       </button>
     );
