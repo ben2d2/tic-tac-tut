5307b05698bcadd92b050a1375c41d808749f8ca
Ben Bridges <ben2d2@gmail.com>
2018-07-13 11:19:22 -0500

    refactor GameInfo table

--- src/components/game_info.js
+++ src/components/game_info.js
@@ -5,33 +5,63 @@ class GameInfo extends React.Component {
     const moves = this.props.history.map((step, move) => {
       var prevMove = (move - 1);
       var token = ((prevMove) % 2) === 0 ? 'X' : 'O'
-      return (
-        <tr key={move}>
-          <td><img className='move-button' onClick={() => this.props.onClick(move)} src={ require('../images/refresh.png') } alt='refresh icon'/></td>
-          <td>{move > 0 ? move : 'Start'}</td>
-          <td>{move > 0 ? this.props.players[token] : ''}</td>
-          <td>{move > 0 ? token : ''}</td>
-          <td>{step.coordinates ? step.coordinates['col'] : ''}</td>
-          <td>{step.coordinates ? step.coordinates['row'] : ''}</td>
-        </tr>
-      )
+      var playerName = this.props.players[token];
+      if (playerName) {
+        return (
+          <tr key={move}>
+            <td><img className='move-button' onClick={() => this.props.onClick(move)} src={ require('../images/refresh.png') } alt='refresh icon'/></td>
+            <td>{move > 0 ? move : 'Start'}</td>
+            <td>{move > 0 ? playerName : ''}</td>
+            <td>{move > 0 ? token : ''}</td>
+            <td>{step.coordinates ? step.coordinates['col'] : ''}</td>
+            <td>{step.coordinates ? step.coordinates['row'] : ''}</td>
+          </tr>
+        )
+      } else {
+        return (
+          <tr key={move}>
+            <td><img className='move-button' onClick={() => this.props.onClick(move)} src={ require('../images/refresh.png') } alt='refresh icon'/></td>
+            <td>{move > 0 ? move : 'Start'}</td>
+            <td>{move > 0 ? token : ''}</td>
+            <td>{step.coordinates ? step.coordinates['col'] : ''}</td>
+            <td>{step.coordinates ? step.coordinates['row'] : ''}</td>
+          </tr>
+        )
+      }
     });
 
-    return (
-      <table>
-        <thead>
-          <tr>
-            <th>Reset</th>
-            <th>Move #</th>
-            <th>Player</th>
-            <th>Token</th>
-            <th>Col</th>
-            <th>Row</th>
-          </tr>
-        </thead>
-        <tbody>{moves}</tbody>
-      </table>
-    )
+    if (this.props.players['X'] && this.props.players['O']) {
+      return (
+        <table>
+          <thead>
+            <tr>
+              <th>Reset</th>
+              <th>Move #</th>
+              <th>Player</th>
+              <th>Token</th>
+              <th>Col</th>
+              <th>Row</th>
+            </tr>
+          </thead>
+          <tbody>{moves}</tbody>
+        </table>
+      )
+    } else {
+      return (
+        <table>
+          <thead>
+            <tr>
+              <th>Reset</th>
+              <th>Move #</th>
+              <th>Token</th>
+              <th>Col</th>
+              <th>Row</th>
+            </tr>
+          </thead>
+          <tbody>{moves}</tbody>
+        </table>
+      )
+    }
   }
 }
 
